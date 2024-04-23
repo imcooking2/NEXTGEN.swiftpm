a4ff5e95ede2fd49c5a3cd887c16c6fee1aeb403
Bryant Lertpradist <Blertpradist4516@stu.d214.org>
2024-04-23 13:28:50 -0500

    named front title "Home"

--- ContentView.swift
+++ ContentView.swift
@@ -5,16 +5,12 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
             
             NavigationView {
                 NavigationLink(destination: Text("Second View")) {
                     Text("Hello, World!")
                 }
-                .navigationTitle("Navigation")
+                .navigationTitle("Home")
             }
         }
     }
