53d32e063c9448fff5a33aec79d385118c81b596
Bryant Lertpradist <Blertpradist4516@stu.d214.org>
2024-04-23 13:01:47 -0500

    made a achievements progress and achievement view

--- AchievementView.swift
+++ AchievementView.swift
@@ -5,4 +5,23 @@
 //  Created by Bryant Lertpradist  on 4/23/24.
 //
 
-import Foundation
+import SwiftUI
+
+struct AchievementBarView: View {
+    @Binding var progress: Double
+    
+    var body: some View {
+        VStack {
+            Text("Achievement Status: \(Int(progress * 100))%")
+                .font(.headline)
+                .padding()
+            
+            ProgressView(value: progress)
+                .frame(height: 20)
+                .padding()
+                .background(Color.gray.opacity(0.2))
+                .cornerRadius(10)
+        }
+    }
+}
+
--- ContentView.swift
+++ ContentView.swift
@@ -1,12 +1,15 @@
 import SwiftUI
 
 struct ContentView: View {
+    @Binding var progress: Double
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            VStack {
+                AchievementBarView(progress: $progress)
+            }
+            .navigationBarTitle("Achievements",displayMode: .inline)
         }
     }
 }
+
